2d9f4e8486e9b6cbbaeac4b24b00b7dc6bea6ea2
Antonina Nevstedt <tonyaaaaa@gmail.com>
2021-04-18 13:34:34 +0200

    workshop.js

---
+++ workshop.js
@@ -0,0 +1,12 @@
+// Welcome to the workshop! We will fill up this file during live coding :)
+
+// Recap of variables and data types
+// Arrays - creation, indexing, length, indexOf, includes
+// Mutation of arrays - push, pop, shift, unshift, reverse
+// Objects - create, mutate, spread
+// Immutable handling of arrays - concat, map, filter, spread
+
+// EXTRA:
+// More methods - some, every, reduce, sort
+// Multidimensional arrays (tic-tac-toe), array of objects (card deck)
+// Array and object descructuring
